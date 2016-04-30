6ebea565fe9553abf8e1f6e61b6ba553fce26550
ga503306 <ga503306@yahoo.com.tw>
2016-04-30 22:55:04 +0800

    Added files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.cbCOM = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.Label();
+            this.sqldata_all = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(197, 23);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(95, 157);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 3;
+            this.btnWrite.Text = "write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(14, 156);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 24);
+            this.btnRead.TabIndex = 4;
+            this.btnRead.Text = "read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(197, 52);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // cbCOM
+            // 
+            this.cbCOM.FormattingEnabled = true;
+            this.cbCOM.Location = new System.Drawing.Point(12, 25);
+            this.cbCOM.Name = "cbCOM";
+            this.cbCOM.Size = new System.Drawing.Size(121, 20);
+            this.cbCOM.TabIndex = 6;
+            this.cbCOM.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(5, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "連線狀況：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(119, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "disconnting";
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Username.Location = new System.Drawing.Point(10, 65);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(110, 19);
+            this.Username.TabIndex = 9;
+            this.Username.Text = "datareceived: ";
+            // 
+            // sqldata_all
+            // 
+            this.sqldata_all.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.sqldata_all.FormattingEnabled = true;
+            this.sqldata_all.ItemHeight = 16;
+            this.sqldata_all.Location = new System.Drawing.Point(2, 186);
+            this.sqldata_all.Name = "sqldata_all";
+            this.sqldata_all.Size = new System.Drawing.Size(283, 84);
+            this.sqldata_all.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.sqldata_all);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbCOM);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ComboBox cbCOM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.ListBox sqldata_all;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Text.RegularExpressions;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        static string dbHost = "localhost";
+        static string dbUser = "root";
+        static string dbPass = "";
+        static string dbName = "userregisterclassroom";
+        static string sqldata;
+
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+            
+
+        }
+
+        private delegate void myUICallBack(string myStr, Control ctl); //跨執行續
+        private void myUI(string myStr, Control ctl)
+        {
+            if (this.InvokeRequired)
+            {
+                myUICallBack myUpdate = new myUICallBack(myUI);
+                this.Invoke(myUpdate, myStr, ctl);
+            }
+            else
+            {
+                ctl.Text = myStr;
+            }
+        }
+
+        string sql(string rfid)
+        {
+            //string connStr = "server=" + dbHost + ";uid=" + dbUser + ";pwd=" + dbPass + ";database=" + dbName + ";CharSet=utf8_general_ci;";
+            string strMySqlServer = String.Format("server=" + dbHost + ";user id=" + dbUser + "; password=" + dbPass + "; database=" + dbName + ";");
+            MySqlConnection conn = new MySqlConnection(strMySqlServer);
+
+            // 連線到資料庫 
+            try
+            {
+                conn.Open();
+                // 進行select
+                string SQL = "SELECT * FROM  `registerclassroom` WHERE  `userid` = "+ rfid;
+                //string SQL = "SELECT userid,data,classroom FROM registerclassroom";
+                //  string SQL = "SELECT userid FROM idpass";
+                //string SQL = "SELECT rfid.rfid FROM `rfid` where rfid = '03D9E6C7'";
+                MessageBox.Show(SQL);
+
+                try
+                {
+                    MySqlCommand cmd = new MySqlCommand(SQL, conn);
+                    MySqlDataReader myData = cmd.ExecuteReader();
+
+                    if (!myData.HasRows)
+                    {
+                        // 如果沒有資料,顯示沒有資料的訊息 
+                        MessageBox.Show("No data.");
+                        return "0";
+                    }
+                    else
+                    {
+                        // 讀取資料並且顯示出來 
+                        while (myData.Read())
+                        {
+                            //label1.Text = myData.GetString(0) + " " ;
+                            //label2.Text = myData.GetString(1) + " " + myData.GetString(2) + " " + myData.GetString(3);
+                            MessageBox.Show(myData.GetString(0) + myData.GetString(1) + myData.GetString(2), "資料");
+                            //return true;
+                        }
+                        myData.Close();
+                        return myData.GetString(0);
+                    }
+                }
+                catch (MySql.Data.MySqlClient.MySqlException ex)
+                {
+                    MessageBox.Show("Error " + ex.Number + " : " + ex.Message);
+                    return "0";
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("無法連線到資料庫.");
+                        break;
+                    case 1042:
+                        MessageBox.Show("無效的主機名稱");
+                        break;
+                    case 1045:
+                        MessageBox.Show("使用者帳號或密碼錯誤,請再試一次.");
+                        break;
+                }
+                return "0";
+            }
+        }
+       
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] serialPorts = SerialPort.GetPortNames();
+
+            foreach (string serialPort in serialPorts)
+            {
+                cbCOM.Items.Add(serialPort);
+                if (cbCOM.Items.Count > 0)
+                {
+                    cbCOM.SelectedIndex = 0;
+                }
+            }
+            
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            sqlsearch_all("SELECT * FROM  `registerclassroom` LIMIT 0 , 30");
+
+        }
+
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = (string)cbCOM.SelectedItem;
+            serialPort1.Open();
+            label2.Text = "connting to " + cbCOM.SelectedItem.ToString();
+            label2.ForeColor = Color.Green;
+
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("1");
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            label2.Text = "disconnting";
+            label2.ForeColor = Color.Red;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void serialPort1_DataReceived(object sender,  EventArgs e)
+        {
+            string data = serialPort1.ReadLine();
+
+            //label1.Text = "datareceived: " + data;
+           
+            data = data.Replace("\r", "").Replace("\n", "");
+            myUI("datareceived: " + data, Username);
+            //data=data.Replace(@"\r\n", ""); 
+            sqldata = sql(data);
+
+
+            if (data.Equals(sqldata))
+            {
+                serialPort1.Write("1");
+                MessageBox.Show("開鎖", data);
+            }
+            else{
+                serialPort1.Write("2");
+                MessageBox.Show("滾", data);
+            }
+            
+         }
+
+
+
+        string sqlsearch_all(string search)
+        {
+            //string connStr = "server=" + dbHost + ";uid=" + dbUser + ";pwd=" + dbPass + ";database=" + dbName + ";CharSet=utf8_general_ci;";
+            string strMySqlServer = String.Format("server=" + dbHost + ";user id=" + dbUser + "; password=" + dbPass + "; database=" + dbName + ";");
+            MySqlConnection conn = new MySqlConnection(strMySqlServer);
+
+            // 連線到資料庫 
+            try
+            {
+                conn.Open();
+                // 進行select
+                string SQL = search;
+                //string SQL = "SELECT userid,data,classroom FROM registerclassroom";
+                //  string SQL = "SELECT userid FROM idpass";
+                //string SQL = "SELECT rfid.rfid FROM `rfid` where rfid = '03D9E6C7'";
+                MessageBox.Show(SQL);
+
+                try
+                {
+                    MySqlCommand cmd = new MySqlCommand(SQL, conn);
+                    MySqlDataReader myData = cmd.ExecuteReader();
+
+                    if (!myData.HasRows)
+                    {
+                        // 如果沒有資料,顯示沒有資料的訊息 
+                        MessageBox.Show("No data.");
+                        return "0";
+                    }
+                    else
+                    {
+                        // 讀取資料並且顯示出來 
+                        while (myData.Read())
+                        {
+
+                            sqldata_all.Items.Add(myData.GetString(0) + "  " + myData.GetString(1) + " " +myData.GetString(2));
+                       
+                            //return true;
+                        }
+                        myData.Close();
+                        return myData.GetString(0);
+                    }
+                }
+                catch (MySql.Data.MySqlClient.MySqlException ex)
+                {
+                    MessageBox.Show("Error " + ex.Number + " : " + ex.Message);
+                    return "0";
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("無法連線到資料庫.");
+                        break;
+                    case 1042:
+                        MessageBox.Show("無效的主機名稱");
+                        break;
+                    case 1045:
+                        MessageBox.Show("使用者帳號或密碼錯誤,請再試一次.");
+                        break;
+                }
+                return "0";
+            }
+        }
+       
+        
+
+    }
+}
